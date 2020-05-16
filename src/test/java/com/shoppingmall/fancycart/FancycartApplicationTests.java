3fddf45cac699248ee254efbfbeb4c37f0e93955
양기현 <rlgusdid@naver.com>
2020-05-16 20:06:35 +0900

    프로젝트 생성

---
+++ src/test/java/com/shoppingmall/fancycart/FancycartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.fancycart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancycartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
